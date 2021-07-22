93a1efcc952f6269888cfe5855d45aacf1b9fe22
LuckyLuke83 <35734205+LuckyLuke83@users.noreply.github.com>
2021-07-22 15:31:28 +0200

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,8 @@
+function navHamburger() {
+  var x = document.getElementById("topNav");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation";
+  }
+}
